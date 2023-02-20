1b4039ba0271f248423ed8a021115c6085dea9b0
Guillaume GILBERT <ggilbert@e1r12p21.clusters.42paris.fr>
2023-02-20 18:28:18 +0100

    need last part of time

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -3,27 +3,30 @@ import time
 
 
 def ft_progress(lst: list):
-    max_lst = max(lst) + 1
+    max_lst = max(lst)
+    start_time = time.perf_counter()
     for i in lst:
         pourcent = round(i * 100 / max_lst)
         print(
-            f'ETA: ??s [{pourcent:3d}%]'
+            f'ETA: {time.perf_counter() - start_time:.2f}s '
+            f'[{pourcent:3d}%]'
             f'[{("="*round(pourcent / 5) + ">"):21}] '
-            f'{i}/{max_lst} | elapsed time ??s'
+            f'{i}/{max_lst} | '
+            f'elapsed time ??s'
             , end="\r"
         )
         yield i
 
 
 if __name__ == "__main__":
-    # listy = range(3333)
+    # listy = range(3334)
     # ret = 0
     # for el in ft_progress(listy):
     #     ret += el
     #     time.sleep(0.005)
     # print()
     # print(ret)
-    listy = range(1000)
+    listy = range(1001)
     ret = 0
     for el in ft_progress(listy):
         ret += (el + 3) % 5
